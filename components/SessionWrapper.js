5723edb0f700943d9f21f54f9fb300708b8ed6c9
gyawaliaadim <110762518+gyawaliaadim@users.noreply.github.com>
2025-04-24 21:27:02 +0545

    Updated Session Provider

--- components/SessionWrapper.js
+++ components/SessionWrapper.js
@@ -1,11 +1,22 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react"
+// import { SessionProvider } from "next-auth/react"
 
-export default function SessionWrapper({ children }) {
+// export default function SessionWrapper({ children }) {
+//   return (
+//     <SessionProvider>
+//       {children}
+//     </SessionProvider>
+//   )
+// }
+import { SessionProvider } from "next-auth/react"
+export default function SessionWrapper({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <SessionProvider>
-      {children}
+    <SessionProvider session={session} basePath="/api/auth">
+      <Component {...pageProps} />
     </SessionProvider>
   )
 }
